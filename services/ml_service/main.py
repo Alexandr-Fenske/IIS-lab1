76149c69f4c8b5445ab81053acdef89c44d45ad4
Alexandr <alexandrfenske@gmail.com>
2024-12-17 18:31:26 +0300

    lr-4

--- services/ml_service/main.py
+++ services/ml_service/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 from api_handler import FastAPIHandler
+from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_client import Histogram, Gauge, Counter, Summary
+
 
 app = FastAPI()
 app.handler = FastAPIHandler()
+
+instrumentator = Instrumentator()
+instrumentator.instrument(app).expose(app)
+
+prediction_metric = Histogram(
+    'prediction_metric_histogram',
+    'histogram of predicted phones',
+    buckets=(0.9,1.9,2.9,3.9)
+)
 
 @app.get('/')
 def root_dir():
@@ -11,9 +23,8 @@ def root_dir():
 @app.post('/api/prediction')
 def make_prediction(phone_id: int, item_features: dict):
     prediction = app.handler.predict(item_features)[0]
-    print('prediction', prediction)
-    print('prediction', type(prediction))
 
+    prediction_metric.observe(prediction)
 
 
     return ({
